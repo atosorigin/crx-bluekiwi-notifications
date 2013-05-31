2756a5a565846f05f3f17e8f77aa330f898deb3d
anthonylau <anthonywaiholau@gmail.com>
2013-05-31 21:49:31 +0800

    add url format validation git#17

--- js/options.js
+++ js/options.js
@@ -1,5 +1,9 @@
 $.ajaxSetup({timeout:10 * 1000}); 
 
+function validateURL(url){
+	return /^(https?):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(url);
+}
+
 function save_options() {
   var evtOptionSrc = 'options';
   $('#url').prop('disabled',true);
@@ -7,36 +11,42 @@ function save_options() {
 
   var bkurl = document.getElementById("url").value;
   
-  bkurl = bkurl.replace(/\/+$/,'');//remove last splash if exists
-  console.log('bkurl ' + bkurl);
-  
-  var notifurl = bkurl + NOTIF_URL;
-  var status = $("#status");
-  var errorhtml = "<div class='alert alert-warning enter-url'>Error while validating blueKiwi URL.  Are you <a href='" + bkurl + "' target='_blank'>logged in</a>?</div>";
-  status.html("<div class='alert alert-info'><img src='img/ajax-loader.gif'/><strong>Validating URL...</strong></div>");
-  $.get(notifurl, function(data){
-		if(typeof data.data === 'undefined'){
-			_gaq.push(['_trackEvent', evtOptionSrc, 'bkurl' , 'invalid']);
-			$("#status").html(errorhtml);
-		}else{
-			chrome.storage.sync.set({'bkurl': bkurl},function(){
-				_gaq.push(['_trackEvent', evtOptionSrc, 'bkurl' , 'valid']);
-				console.log('bkurl saved with ' + bkurl);
-				status.html("<div class='alert alert-success'><strong>Confirmed!</strong> URL Saved!</div>");
-				setTimeout(function() {
-					status.html('');
-				}, 2000);
-			});
-		}
-		$('#url').prop('disabled',false);
+  if(!validateURL(bkurl)){
+    $("#status").html("<div class='alert alert-warning enter-url'>Invalid URL format.");
+    $('#url').prop('disabled',false);
 		$('#save').prop('disabled',false);
-  }).fail(function(jqXHR, textStatus, errorThrown){
-		status.html(errorhtml);
-		$('#url').prop('disabled',false);
-		$('#save').prop('disabled',false);
-		console.error('unable to get notifiation data from %s. Error Message: %s', bkurl, textStatus);
-		_gaq.push(['_trackEvent', evtOptionSrc, 'bkurl' , 'error-' + textStatus]);
-  });
+  }else{
+	  bkurl = bkurl.replace(/\/+$/,'');//remove last splash if exists
+	  console.log('bkurl ' + bkurl);
+	  
+	  var notifurl = bkurl + NOTIF_URL;
+	  var status = $("#status");
+	  var errorhtml = "<div class='alert alert-warning enter-url'>Error while validating blueKiwi URL.  Are you <a href='" + bkurl + "' target='_blank'>logged in</a>?</div>";
+	  status.html("<div class='alert alert-info'><img src='img/ajax-loader.gif'/><strong>Validating URL...</strong></div>");
+	  $.get(notifurl, function(data){
+			if(typeof data.data === 'undefined'){
+				_gaq.push(['_trackEvent', evtOptionSrc, 'bkurl' , 'invalid']);
+				$("#status").html(errorhtml);
+			}else{
+				chrome.storage.sync.set({'bkurl': bkurl},function(){
+					_gaq.push(['_trackEvent', evtOptionSrc, 'bkurl' , 'valid']);
+					console.log('bkurl saved with ' + bkurl);
+					status.html("<div class='alert alert-success'><strong>Confirmed!</strong> URL Saved!</div>");
+					setTimeout(function() {
+						status.html('');
+					}, 2000);
+				});
+			}
+			$('#url').prop('disabled',false);
+			$('#save').prop('disabled',false);
+	  }).fail(function(jqXHR, textStatus, errorThrown){
+			status.html(errorhtml);
+			$('#url').prop('disabled',false);
+			$('#save').prop('disabled',false);
+			console.error('unable to get notifiation data from %s. Error Message: %s', bkurl, textStatus);
+			_gaq.push(['_trackEvent', evtOptionSrc, 'bkurl' , 'error-' + textStatus]);
+	  });
+   }
 }
 
 function restore_options() {
