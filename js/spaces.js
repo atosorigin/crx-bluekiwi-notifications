a0a58e6bd9717e49c0e4285b770b24955ad0838f
anthonylau <anthonywaiholau@gmail.com>
2013-07-06 18:19:03 +0800

    use jquery when then instead of nested function in space.js

--- js/spaces.js
+++ js/spaces.js
@@ -2,35 +2,35 @@ $.ajaxSetup({timeout:30 * 1000});
 
 function loadSpaces(bkurl){
 
-	$.get(bkurl,'html',function(data){
-	   var tmp = $(data).find('a[href^="/user/in"]').attr('href');
+	$.get(bkurl,{ dataType: 'html' })
+  .then(function(data){
+    var tmp = $(data).find('a[href^="/user/in"]').attr('href');
 	   console.log(tmp);
 	   var userID = /\/user\/in\/(.*)\/.*/g.exec(tmp)[1];
 	   var spacesURL = bkurl + '/user/in/' + userID + '/settings/spaces';
 	
-	  $.get(spacesURL, function(data){
-	    var spacesTable = $(data);
-	    var spacelist = $('#space-list');
-	      console.log(spacesTable); 
-	    spacesTable.find('tr td:nth-child(1)').each(function(idx,el){
-	      var spaceURL = bkurl + $(el).find('a').attr('href');
-        var spaceName = $(el).find('a').text();
-        var spaceType = $(el).find('.space_type').text();
-	      console.log(spaceURL);
-	      var space = $('<a></a>').attr('href', spaceURL)
-          .attr('target', '_blank').html(spaceName + '[' + spaceType + ']');
-	      $('#space-listtbl tbody').append('<tr><td></td></tr>');
-	      $('#space-listtbl tbody tr:last td').append(space);
-	    });
-	    
-	    $('#space-list').show();
-	    $('#loading').hide();
-	  },'html')
-	  .fail(function(jqXHR, textStatus, errorThrown){
+	  return $.get(spacesURL,{ dataType: 'html' });
+  }).done(function(data){
+    var spacesTable = $(data);
+    var spacelist = $('#space-list');
+      console.log(spacesTable); 
+    spacesTable.find('tr td:nth-child(1)').each(function(idx,el){
+      var spaceURL = bkurl + $(el).find('a').attr('href');
+      var spaceName = $(el).find('a').text();
+      var spaceType = $(el).find('.space_type').text();
+      console.log(spaceURL);
+      var space = $('<a></a>').attr('href', spaceURL)
+        .attr('target', '_blank').html(spaceName + '[' + spaceType + ']');
+      $('#space-listtbl tbody').append('<tr><td></td></tr>');
+      $('#space-listtbl tbody tr:last td').append(space);
+    });
+    
+    $('#space-list').show();
+    $('#loading').hide();
+	}).fail(function(jqXHR, textStatus, errorThrown){
 	    console.log('failed to fetch space list, ' + textStatus + '-' + errorThrown);
 	    _gaq.push(['_trackEvent', 'space-list' , 'fail' , textStatus + '-' + errorThrown]);
-	  });
-  });
+	});
 }
 
 document.addEventListener('DOMContentLoaded', function () {
