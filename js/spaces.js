45aa2ca912f9a26ef37a80e09c66d0b0bc88ad8e
anthonylau <anthonywaiholau@gmail.com>
2013-07-07 17:10:46 +0800

    add spaces cache and sort space by name

--- js/spaces.js
+++ js/spaces.js
@@ -6,7 +6,6 @@ function loadSpaces(bkurl){
 	$.get(pageURL,{ dataType: 'html' })
   .then(function(data){
     var tmp = $(data).find('a[href^="/user/in"][href*="settings"]').attr('href');
-	  console.log(tmp);
 	  var userID = /\/user\/in\/(.*)\/settings(.*)/g.exec(tmp)[1];
 	  var spacesURL = bkurl + '/user/in/' + userID + '/settings/spaces';
     console.log('userID='+userID);
@@ -14,19 +13,23 @@ function loadSpaces(bkurl){
   }).done(function(data){
     var spacesTable = $(data);
     var spacelist = $('#space-list');
-      console.log(spacesTable); 
+    var spaces = [];
     spacesTable.find('tr td:nth-child(1)').each(function(idx,el){
-      var spaceURL = bkurl + $(el).find('a').attr('href');
-      var spaceName = $(el).find('a').text();
-      var spaceType = $(el).find('.space_type').text();
-      console.log(spaceURL);
-      var space = $('<a></a>').attr('href', spaceURL)
-        .attr('target', '_blank').html(spaceName + '[' + spaceType + ']');
-      $('#space-listtbl tbody').append('<tr><td></td></tr>');
-      $('#space-listtbl tbody tr:last td').append(space);
+      spaces.push({
+        spaceURL: bkurl + $(el).find('a').attr('href'),
+        spaceName: $(el).find('a').text(),
+        spaceType: $(el).find('.space_type').text()
+      });
     });
-    
-    $('#space-list').show();
+    spaces.sort(function(a,b){
+      if (a.spaceName < b.spaceName)
+        return -1;
+      if (a.spaceName > b.spaceName)
+        return 1;
+      return 0;
+    });
+    chrome.storage.local.set({bkSpacesCache: spaces});
+    renderSpaces(spaces);
     $('#loading').hide();
 	}).fail(function(jqXHR, textStatus, errorThrown){
 	    console.log('failed to fetch space list, ' + textStatus + '-' + errorThrown);
@@ -37,10 +40,32 @@ function loadSpaces(bkurl){
 	});	
 }
 
+function renderSpaces(spaces){
+  if(spaces){
+    $('#space-listtbl-body').empty();
+    for(var i=0; i<spaces.length; i++){
+      var spaceURL= spaces[i].spaceURL;
+      var spaceName = spaces[i].spaceName;
+      var spaceType = spaces[i].spaceType;
+      var space = $('<a></a>').attr('href', spaceURL)
+        .attr('target', '_blank').html(spaceName + '[' + spaceType + ']');
+      $('#space-listtbl-body').append(
+        $('<tr></tr>').append(
+          $('<td></td>').append(space)
+        )
+      );
+    }
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
 	console.log("DOMContentLoaded");
 	chrome.storage.sync.get('bkurl', function(items){
     loadSpaces(items.bkurl);
+	});
+  chrome.storage.local.get('bkSpacesCache', function(items){
+    console.log(items);
+    renderSpaces(items.bkSpacesCache);
 	});
 	$('#btnHome').click(function(){
     _gaq.push(['_trackEvent', 'popup-home-btn', 'clicked']);
