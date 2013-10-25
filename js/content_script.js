402924805f076cf931e0042305786f2285d3bcba
anthonylau <anthonywaiholau@gmail.com>
2013-10-25 21:33:41 +0800

    use footer nav if heading is not available

--- js/content_script.js
+++ js/content_script.js
@@ -10,10 +10,26 @@
 
 (function(){
   console.log('content_script start');
-  var spaceTitle = document.querySelectorAll('h2 > .max70.ellipsis');
   
-  var nav1 = spaceTitle[0].text.replace(/(\r\n|\n|\r)/gm,"").replace(/\s+/gm,' ');
-  document.querySelector('title').innerHTML = (nav1).trim();
+  var origTitle = document.querySelector('title').innerHTML;
+  var title = origTitle;
+  
+  var spaceTitle = $('h2 > .max70.ellipsis');
+  var navItems = document.querySelectorAll('.page_footer > .railway > li');
+
+  if(spaceTitle.text()){
+    var nav1 = spaceTitle.text().replace(/(\r\n|\n|\r)/gm,"").replace(/\s+/gm,' ').trim();
+    //var nav2 = spaceTitle.parent().contents().filter(function(){return this.nodeType == 3;}).last().text().replace(/(\r\n|\n|\r|\/)/gm,"").replace(/\s+/gm,' ').trim();
+    //not work if the sub title has link
+    //title = nav1 + ' » ' + nav2;
+    title = nav1;
+  }else if (navItems){
+    var nav1 = navItems[1]?navItems[1].textContent.replace(/(\r\n|\n|\r)/gm,"").replace(/\s+/gm,' '):'';
+    var nav2 = navItems[2]?navItems[2].textContent.replace(/(\r\n|\n|\r)/gm,"").replace(/\s+/gm,' '):'';
+    title = (nav1 + nav2).trim();
+  }
+  
+  document.querySelector('title').innerHTML = title;
   console.log('content_script end');
 })();
 
