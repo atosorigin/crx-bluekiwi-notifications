195f7c59b44c31c64bac064c142d1803c987651d
anthonylau <anthonywaiholau@gmail.com>
2013-10-25 21:35:08 +0800

    content script clean up

--- js/content_script.js
+++ js/content_script.js
@@ -1,17 +1,6 @@
-/*(function(){
-  console.log('content_script start');
-  var navItems = document.querySelectorAll('.page_footer > .railway > li');
-  var nav1 = navItems[1].textContent.replace(/(\r\n|\n|\r)/gm,"").replace(/\s+/gm,' ');
-  var nav2 = navItems[2].textContent.replace(/(\r\n|\n|\r)/gm,"").replace(/\s+/gm,' ');
-  document.querySelector('title').innerHTML = (nav1 + nav2).trim();
-  console.log('content_script end');
-})();*/
-
+var origTitle = document.querySelector('title').innerHTML;
 
 (function(){
-  console.log('content_script start');
-  
-  var origTitle = document.querySelector('title').innerHTML;
   var title = origTitle;
   
   var spaceTitle = $('h2 > .max70.ellipsis');
@@ -30,9 +19,5 @@
   }
   
   document.querySelector('title').innerHTML = title;
-  console.log('content_script end');
 })();
 
-
-//$('.page_footer > .railway > li').slice(1,3).text().replace(/(\r\n|\n|\r)/gm,"").replace(/\s+/gm,' ').trim();
-//$('h2 > .max70.ellipsis').parent().text().replace(/(\r\n|\n|\r)/gm,"").replace(/\s+/gm,' ').trim();
