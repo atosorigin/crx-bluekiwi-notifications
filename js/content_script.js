313a6ee0fb1e2ab86be56eba6d50bda374a4b6b6
anthony <anthonywaiholau@gmail.com>
2013-11-07 09:39:28 +0800

    fix title enhancement not reset to page title after closed modale_preview

--- js/content_script.js
+++ js/content_script.js
@@ -8,7 +8,8 @@
   });
   
   var injectTitleEnhanceScript = function(){
-    var title = origTitle;
+    var pageTitle = origTitle;
+    var title = pageTitle;
     
     var navTabActive = $('.nav-tabs > .active').text().trim();
     var spaceTitle = $('h2 > .max70.ellipsis');
@@ -16,30 +17,29 @@
     var feedTitle = $('#feedTitle').text().trim();
 
     if(navTabActive){
-      title = navTabActive;
+      pageTitle = navTabActive;
     }else if(spaceTitle.text()){
       var nav1 = spaceTitle.text().replace(/(\r\n|\n|\r)/gm,"").replace(/\s+/gm,' ').trim();
       //var nav2 = spaceTitle.parent().contents().filter(function(){return this.nodeType == 3;}).last().text().replace(/(\r\n|\n|\r|\/)/gm,"").replace(/\s+/gm,' ').trim();
       //not work if the sub title has link
       //title = nav1 + ' » ' + nav2;
-      title = nav1;
+      pageTitle = nav1;
     }else if (navItems){
       var nav1 = navItems[1]?navItems[1].textContent.replace(/(\r\n|\n|\r)/gm,"").replace(/\s+/gm,' '):'';
       var nav2 = navItems[2]?navItems[2].textContent.replace(/(\r\n|\n|\r)/gm,"").replace(/\s+/gm,' '):'';
-      title = (nav1 + nav2).trim();
+      pageTitle = (nav1 + nav2).trim();
     }
     
+    title = pageTitle;
+    
     if(feedTitle){
-      title += ' » ' + feedTitle.trim();
+      title = pageTitle + ' » ' + feedTitle.trim();
     }
     
     document.querySelector('title').innerHTML = title;
-    origTitle = title;
     
-    //reset title when the popup is removed
-    // select the target node
     var target = document.querySelector('body');
-    // create an observer instance
+    //monitor popup
     new MutationObserver(function(mutations) {
       mutations.forEach(function(mutation) {
         //console.log(mutation);
@@ -47,24 +47,36 @@
           for(var i=0; i<mutation.removedNodes.length; i++){
             var node = mutation.removedNodes[i];
             if(node.id && node.id === 'modale_preview'){
-              document.querySelector('title').innerHTML = origTitle;
+              document.querySelector('title').innerHTML = title;
               break;
             }
           }
         }else if(mutation.target.id && mutation.target.id === 'modale_preview'){
-          var title = origTitle;
+          var newTitle = title;
           var postTitle = $(mutation.target).find('.post_title');
           var h1 = $(mutation.target).find('h1');
           if(postTitle){
-            title = postTitle.text();
+            newTitle = postTitle.text();
           }else if(h1){
-            title = h1.text();
+            newTitle = h1.text();
           }
-          document.querySelector('title').innerHTML = title;
+          document.querySelector('title').innerHTML = newTitle;
         }
       });    
     }).observe(target, {childList: true, subtree: true});
-
+    
+    //monitor feedTitle changes
+    new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        //console.log(mutation);
+        if(mutation.type === 'characterData'){
+          //console.log('feed title change = ' + mutation.target.textContent.trim());
+          feedTitle = mutation.target.textContent.trim();
+          title = origTitle + ' » ' + feedTitle;
+          document.querySelector('title').innerHTML = title;
+        }
+      })
+    }).observe(document.querySelector('#feedTitle'),{subtree: true, characterData: true});
   };
   
   var injectItemFeedCountFaviconScript = function(){
